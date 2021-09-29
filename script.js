7ed3c244d5e523e39cc373534cd31b9f48ad607f
Maskibow <iszroil@gmail.com>
2021-09-29 17:19:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,103 @@
-// CODELAB: Add event listener for beforeinstallprompt event
-window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+/**
+ * Copyright 2015 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
-// CODELAB: Add code to save event & show the install button.
-deferredInstallPrompt = evt;
-installButton.removeAttribute('hidden');
+(function() {
+  var nativeAddAll = Cache.prototype.addAll;
+  var userAgent = navigator.userAgent.match(/(Firefox|Chrome)\/(\d+\.)/);
 
-/ CODELAB: Add code show install prompt & hide the install button.
-deferredInstallPrompt.prompt();
-// Hide the install button, it can't be called twice.
-evt.srcElement.setAttribute('hidden', true);
+  // Has nice behavior of `var` which everyone hates
+  if (userAgent) {
+    var agent = userAgent[1];
+    var version = parseInt(userAgent[2]);
+  }
 
-// CODELAB: Log user response to prompt.
-deferredInstallPrompt.userChoice
-    .then((choice) => {
-      if (choice.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt', choice);
-      } else {
-        console.log('User dismissed the A2HS prompt', choice);
+  if (
+    nativeAddAll && (!userAgent ||
+      (agent === 'Firefox' && version >= 46) ||
+      (agent === 'Chrome'  && version >= 50)
+    )
+  ) {
+    return;
+  }
+
+  Cache.prototype.addAll = function addAll(requests) {
+    var cache = this;
+
+    // Since DOMExceptions are not constructable:
+    function NetworkError(message) {
+      this.name = 'NetworkError';
+      this.code = 19;
+      this.message = message;
+    }
+
+    NetworkError.prototype = Object.create(Error.prototype);
+
+    return Promise.resolve().then(function() {
+      if (arguments.length < 1) throw new TypeError();
+
+      // Simulate sequence<(Request or USVString)> binding:
+      var sequence = [];
+
+      requests = requests.map(function(request) {
+        if (request instanceof Request) {
+          return request;
+        }
+        else {
+          return String(request); // may throw TypeError
+        }
+      });
+
+      return Promise.all(
+        requests.map(function(request) {
+          if (typeof request === 'string') {
+            request = new Request(request);
+          }
+
+          var scheme = new URL(request.url).protocol;
+
+          if (scheme !== 'http:' && scheme !== 'https:') {
+            throw new NetworkError("Invalid scheme");
+          }
+
+          return fetch(request.clone());
+        })
+      );
+    }).then(function(responses) {
+      // If some of the responses has not OK-eish status,
+      // then whole operation should reject
+      if (responses.some(function(response) {
+        return !response.ok;
+      })) {
+        throw new NetworkError('Incorrect response status');
       }
-      deferredInstallPrompt = null;
+
+      // TODO: check that requests don't overwrite one another
+      // (don't think this is possible to polyfill due to opaque responses)
+      return Promise.all(
+        responses.map(function(response, i) {
+          return cache.put(requests[i], response);
+        })
+      );
+    }).then(function() {
+      return undefined;
     });
+  };
 
-// CODELAB: Add event listener for appinstalled event
-window.addEventListener('appinstalled', logAppInstalled);
-
-// CODELAB: Add code to log the event
-console.log('Weather App was installed.', evt);
+  Cache.prototype.add = function add(request) {
+    return this.addAll([request]);
+  };
+}());
