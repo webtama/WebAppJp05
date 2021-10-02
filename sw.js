9fec7f6f81f2c0e5ffb8c3365b61765b75f39178
Maskibow <iszroil@gmail.com>
2021-10-03 01:08:17 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,3 @@
-var APP_PREFIX = 'WebAppJp05_';
-var VERSION = 'version_01';
-var URLS = '.';
-
-const NOT_FOUND_CACHE_FILES = '/WebAppJp05/404.html';
-const CACHE_NAME = 'offline';
-const OFFLINE_URL = '/WebAppJp05/offline/index.html';
-const NOT_FOUND_URL = '/WebAppJp05/404.html';
-
-self.addEventListener('fetch', function(event) {
-  // console.log('[Service Worker] Fetch', event.request.url);
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResponse = await event.preloadResponse;
-        if (preloadResponse) {
-          return preloadResponse;
-        }
-
-        const networkResponse = await fetch(event.request);
-        return networkResponse;
-      } catch (error) {
-        console.log('[Service Worker] Fetch failed; returning offline page instead.', error);
-
-        const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(OFFLINE_URL);
-        return cachedResponse;
-      }
-    })());
-  }
-});
-
 // This is the "Offline page" service worker
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
