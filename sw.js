3aacac0f41a2270d70038e8f5f77bf79caf0ce72
Maskibow <iszroil@gmail.com>
2021-10-05 00:00:16 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -194,10 +194,6 @@ workbox.routing.registerRoute(
 
 // This is the "serving cached media" service worker
 
-const CACHE = "pwabuilder-offline";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
