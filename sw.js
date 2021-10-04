b8814d4d22b1061e4baf33c542d629febc61c161
Maskibow <iszroil@gmail.com>
2021-10-05 00:18:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -192,3 +192,25 @@ workbox.routing.registerRoute(
     ],
   })
 );
+
+// This is the "serving cached media" service worker
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+workbox.loadModule('workbox-cacheable-response');
+workbox.loadModule('workbox-range-requests');
+
+workbox.routing.registerRoute(
+  /.*\.mp4/,
+  new workbox.strategies.CacheFirst({
+    cacheName: CACHE,
+    plugins: [
+      new workbox.cacheableResponse.CacheableResponsePlugin({statuses: [200]}),
+      new workbox.rangeRequests.RangeRequestsPlugin(),
+    ],
+  }),
+);
